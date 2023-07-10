b8b14b70b9e8467c288df5f2c989849ad278205c
Bilal Alsilwadi <balsilwadi@gmail.com>
2023-07-10 17:56:19 -0500

    completed the first 3 steps

---
+++ src/components/Title/Title.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Title.css';
+
+function Title() {
+  return (
+    <div className='Title'>
+      <h1>CHICAGOTOUR</h1>
+      <div className='Title-Subtitle'>Places to Visit in Chicago!</div>
+    </div>
+  )
+}
+
+export default Title
